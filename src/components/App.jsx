2ff855114663cc4ad3f1af9e0804d45bc51a2884
Matthew Cook <mc255v@gmail.com>
2019-12-06 15:26:42 +0900

    updated router with history

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
+import history from '../history';
 import Navbar from './navbar/Navbar';
 import CreatorsList from './creators/CreatorsList';
-import Profile from './profile/Profile';
+import Profile from './profile/ProfileMain';
+import WorksMain from './works/WorksMain';
+import NotFound from './notfound/NotFound';
+import Footer from '../footer/Footer';
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router history={history}>
         <Navbar />
         <Switch>
           <Route exact path="/" component={CreatorsList} />
           <Route exact path="/creator/:id" component={Profile} />
+          <Route exact path="/works/:id" component={WorksMain} />
+          <Route exact path="/notfound" component={NotFound} />
         </Switch>
-      </BrowserRouter>
+        <Footer />
+      </Router>
     </div>
   );
 }
