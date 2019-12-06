238963fd05c278492f06126885532ad3f872315f
Matthew Cook <mc255v@gmail.com>
2019-12-06 15:30:06 +0900

    added footer

---
+++ src/footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <p>foriio <span>Portfolio made easy</span></p>
+      <div>
+        <ul>
+          <li>Terms of use</li>
+          <li>Privacy Policy</li>
+          <li>Discover</li>
+          <li>Blog</li>
+          <li>What's foriio?</li>
+        </ul>
+      </div>
+      <p>copyright</p>
+    </div>
+  );
+}
+
+export default Footer;
