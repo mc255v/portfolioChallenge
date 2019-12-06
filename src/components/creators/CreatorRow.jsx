c1fb2324e6e973972d548a2d130ebed02c4451fe
Matthew Cook <mc255v@gmail.com>
2019-12-06 18:31:04 +0900

    creatorlist finished, beginning pagination

--- src/components/creators/CreatorRow.jsx
+++ src/components/creators/CreatorRow.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
+import history from "../../history";
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import '../../sass/components/CreatorRow.scss';
 
 const Creator = ({ creator }) => {
   const defaultAvatar = "https://dyci7co52mbcc.cloudfront.net/static/images/default_avatar_v2.png";
 
-  return (
-    <div className="creator__container">
-      <div className="creator__profile">
-        <img src={creator.avatar || defaultAvatar} alt="avatar" className="creator__image"/>
-        <div className="creator_role-container">
-          <p className="creator__profession">{creator.profession}</p>
-          <p className="creator__name">{creator.name}</p>
-        </div>
-      </div>
+  const creatorClick = (id) => {
+    history.push(`/creator/${id}`);
+  }
 
-      <div className="creator__works">
+  const workClick = (id) => {
+    history.push(`/works/${id}`);
+  }
+
+  return (
+    <>
+      <Col lg={3}>
+        <div className="creator-col" onClick={() => creatorClick(creator.screen_name)}>
+          <figure className="creator-col__shape">
+            <img src={creator.avatar || defaultAvatar} alt="avatar" className="creator-col__image"/>
+          </figure>
+          <div className="creator-col__info-box">
+            <p className="creator-col__profession">{creator.profession}</p>
+            <p className="creator-col__name">{creator.name}</p>
+          </div>
+        </div>
+      </Col>
+      <Col>
+      <div className="creator-works">
         {creator.works.map(work => {
           return (
-            <img key={work.id} src={work.thumbnail} alt={work.title}/>
+            <img 
+              key={work.id} 
+              src={work.thumbnail} 
+              alt={work.title} 
+              onClick={() => workClick(work.id)}
+              className="creator-works__image"/>
           );
         })}
       </div>
-    </div>
+      </Col>
+    </>
   );
 }
 
--- src/components/creators/CreatorsList.jsx
+++ src/components/creators/CreatorsList.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getCreators } from '../../actions/actions'
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import Creator from './CreatorRow';
 import Pagination from '../pagination/Pagination';
+
+import '../../sass/components/CreatorList.scss';
 
 const CreatersList = () => {
   const creatorsFullList = useSelector(state => state.creatorsFullList);
@@ -14,13 +19,21 @@ const CreatersList = () => {
   }, [])
 
   return (
-    <div className="creatorlist__container">
-      <h1 className="creatorlist__title">Picked up creators</h1>
-      <h3 className="creatorlist__subtitle">Picked up creators</h3>
-      {paginationList.map((creator) => {
-        return  <Creator key={creator.id} creator={creator}/>
-      })}
-      <Pagination totalRecords={creatorsFullList.length} />
+    <div className="creator-list">
+      <Container>
+        <div className="creator-list__header">
+          <h1 className="creator-list__title">Picked up creators</h1>
+          <h3 className="creator-list__subtitle">Picked up creators</h3>
+        </div>
+        {paginationList.map((creator) => {
+          return  (
+            <Row key={creator.id} className="creator-list__row">
+              <Creator  creator={creator}/>
+            </Row>
+          );
+        })}
+        <Pagination totalRecords={creatorsFullList.length} />
+      </Container>
     </div>
   );
 }
