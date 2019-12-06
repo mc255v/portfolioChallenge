03c4fc731bf0df2d58f60fc7eff623c4c1f00048
Matthew Cook <mc255v@gmail.com>
2019-12-06 18:05:11 +0900

    navbar styling done, no functionality

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,15 +5,17 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar__left">
-        <div className="navbar_logo">foriio</div>
-        <div className="navbar__search-select">Creator</div>
-        <input type="search" className="navbar__search-input" />
-        <button className="btn navbar__search-btn">Search</button>
+        <div className="navbar__logo">foriio</div>
+        <div className="navbar__search-select">
+          <p>Creators</p>
+        </div>
+        <input type="search" className="navbar__search-input" placeholder="Let's discover great creatives"/>
+        <button className="navbar__search-btn">Search</button>
       </div>
       <div className="navbar__right">
-        <button className="btn">Make yours free</button>
-        <button className="btn">Log in</button>
-        <button className="btn">EN</button>
+        <button className="navbar__btn navbar__btn--primary">Make yours for free</button>
+        <button className="navbar__btn">Log in</button>
+        <button className="navbar__btn">EN</button>
       </div>
     </nav>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store.js'
 import App from './components/App';
+import './sass/abstracts/base.scss';
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>, 
